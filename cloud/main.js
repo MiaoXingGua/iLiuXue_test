84a1e89775b4d7a2c87ed415e932d87fd9d83edb
zerozaki0752 <albert@AlberttekiMacBook-Pro.local>
2013-09-07 20:51:54 +0800

    时事
    s

--- cloud/main.js
+++ cloud/main.js
@@ -3,16 +3,15 @@
 //var name = require('cloud/name.js');
 //var user = require('cloud/user.js')
 
-var XMPP_SEVER = "http://115.28.44.100:7070/http-bind/";
-var XMPP_HOST = "@115.28.44.100";
+//var XMPP_SEVER = "http://115.28.44.100:7070/http-bind/";
+//var XMPP_HOST = "@115.28.44.100";
 
-var url = require('cloud/url.js')
-var strophe = require('./strophejs/strophe.js')
+//var url = require('cloud/url.js')
+//var strophe = require('./strophejs/strophe.js')
 
 AV.Cloud.define("hello", function(request, response) {
   response.success("hello !" + request.params.name);
 });
-
 
 
 AV.Cloud.define('register', function(request, response) {
@@ -43,94 +42,5 @@ AV.Cloud.define('register', function(request, response) {
                 }
         });
     }
-});
-
-AV.Cloud.define('pingXMPP', function(request, response) {
-                
-                
-
-
-
-//连接到xmpp服务器
-var xmppConnect = new Strophe.Connection(XMPP_SEVER);
-
-xmppConnect.connect(data.jid, data.password, function (status) {
-                    if (status === Strophe.Status.CONNECTED) {
-                    $(document).trigger('connected');
-                    } else if (status === Strophe.Status.DISCONNECTED) {
-                    $(document).trigger('disconnected');
-                    }
-                    });
-
-conn.connect(data.jid, data.password, function (status) {
-             if (status === Strophe.Status.CONNECTED) {
-             $(document).trigger('connected');
-             } else if (status === Strophe.Status.DISCONNECTED) {
-             $(document).trigger('disconnected');
-             }
-             });
-
-$(document).bind('connected', function () {
-                 // inform the user
-                 Hello.log("Connection established.");
-                 Hello.connection.addHandler(Hello.handle_pong, null, "iq", null, "ping1");
-                 var domain = Strophe.getDomainFromJid(Hello.connection.jid);
-                 Hello.send_ping(domain);
-                 });
-
-
-$(document).bind('disconnected', function () {
-                 Hello.log("Connection terminated.");
-                 // remove dead connection object
-                 Hello.connection = null;
-                 });
-
-send_ping: function (to) {
-    var ping = $iq({to: to, type: "get", id: "ping1"}).c("ping", {xmlns: "urn:xmpp:ping"});
-    
-    
-    Hello.log("Sending ping to " + to + ".");
-    
-    
-    Hello.start_time = (new Date()).getTime();
-    Hello.connection.send(ping);
-},
-
-
-handle_pong: function (iq) {
-    var elapsed = (new Date()).getTime() - Hello.start_time;
-    Hello.log("Received pong from server in " + elapsed + "ms.");
-    
-    
-    Hello.connection.disconnect();
-    
-    return false;
-}
-                
-                });
-
-function signUpToXMPP(userId,password){
-    
-    AV.Cloud.httpRequest({
-                         method:'POST'
-
-                         url: "http://" + XMPP_SEVER + "/",
-
-                         headers:{
-                            
-                         
-                         },
-                         
-                         body{
-                         
-                         }
-                         success: function(httpResponse) {
-
-                         },
-                         error: function(httpResponse) {
-                         
-                         }
-    });
-    
 });
 
