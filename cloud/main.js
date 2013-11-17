62af294478859eba3060cb183a2a0308b58de0a8
zerozaki0752 <albert@AlberttekiMacBook-Pro.local>
2013-11-17 12:11:37 +0800

    赞or收藏

--- cloud/main.js
+++ cloud/main.js
@@ -574,51 +574,26 @@ AV.Cloud.afterUpdate("UserFavicon", function(request) {
 
 
         var userFavicon = user.get('userFavicon');
-//        console.dir(userFavicon);
         var userTFR = userFavicon.relation('threads');
-//        console.dir(userFR);
         return userTFR.query().find();
 //        return userFR.query().count();
 
         }).then(function(objects){
+
             //收藏主题or取消收藏主题
-//            console.log(objects.length);
             var userCount = user.get('userCount');
-//            console.dir(userCount);
             userCount.set('numberOfFavicon',objects.length);
-        return userCount.save();
+            return userCount.save();
 
         }).then(function(userCount) {
-            console.dir(userCount);
-            console.log("收藏成功！");
-        //赞回复or取消赞回复
 
-        },function(error){
+            var userFavicon = user.get('userFavicon');
+            var userSFR = userFavicon.relation('supports');
+            return userSFR.query().find();
+//        return userSFR.query().count();
 
-            console.log("收藏失败！");
+        }).then(function(objects) {
 
-    });
-});
-
-//赞回复or取消赞回复
-AV.Cloud.afterUpdate("UserFavicon", function(request) {
-    var user = request.user;
-    var userFavicon = request.object;
-
-    var userQ = new AV.Query(User);
-    userQ.equalTo("objectId", user.id);
-    userQ.include('userCount');
-    userQ.include('userFavicon');
-    userQ.first().then(function(user){
-
-        var userFavicon = user.get('userFavicon');
-//        console.dir(userFavicon);
-        var userTFR = userFavicon.relation('threads');
-//        console.dir(userFR);
-        return userTFR.query().find();
-//        return userFR.query().count();
-
-    }).then(function(objects){
             //收藏主题or取消收藏主题
 //            console.log(objects.length);
             var userCount = user.get('userCount');
@@ -627,13 +602,14 @@ AV.Cloud.afterUpdate("UserFavicon", function(request) {
             return userCount.save();
 
         }).then(function(userCount) {
+
             console.dir(userCount);
-            console.log("赞成功！");
-            //赞回复or取消赞回复
+            console.log("收藏or赞成功！");
 
         },function(error){
 
-            console.log("赞失败！");
+            console.log("收藏or赞失败！");
 
-        });
+    });
 });
+
